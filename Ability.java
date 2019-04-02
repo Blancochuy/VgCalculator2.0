7ec6f03474441bd17a9ce259764e69b7e1cb7040
blancochuy <Blancochuy@me.com>
2019-04-02 15:50:33 -0600

    update

--- Ability.java
+++ Ability.java
@@ -1,17 +1,32 @@
 /*
 Autor: Chuy BC
-Fecha de inicio: 4/2/19 
+Fecha de inicio: 4/2/19
 Fecha de terminación:
 Objetivo del Programa:
 */
-class Ability
+import java.io.Serializable;
+public class Ability implements Serializable
 {
 	public float wpRatio;
 	public float cpRatio;
 	public int damageAbility;
-	
+
 	public Ability(float wpRatio, float cpRatio, int damageAbility)
 	{
-		
+		this.wpRatio = wpRatio;
+		this.cpRatio = cpRatio;
+		this.damageAbility = damageAbility;
+	}
+	public float getWpRatio()
+	{
+		return wpRatio;
+	}
+	public float getCpRatio()
+	{
+		return cpRatio;
+	}
+	public int damageAbility()
+	{
+		return damageAbility;
 	}
 }
--- Hero.java
+++ Hero.java
@@ -1,24 +1,34 @@
 /*
 Autor: Chuy BC
-Fecha de inicio: 4/2/19 
+Fecha de inicio: 4/2/19
 Fecha de terminación: ~
 Objetivo del Programa:class hero vainglory
 */
-class Hero
+import java.io.Serializable;
+public class Hero implements Serializable
 {
+	public String heroName;
 	public float health;
 	public float weaponDamage;
 	public float armor;
 	public float shield;
 	public Ability[] heroAblty;
-	
-	public Hero(float health, float weaponDamage, float armor, float shield, Ability[] heroAblty)
+
+	ArrayList<Ability> heroeAbilies  = new ArrayList<Ability>();
+
+	public Hero(String heroName, float health, float weaponDamage, float armor, float shield, Ability[] heroAblty)
 	{
+		this.heroName = heroName;
 		this.health = health;
 		this.weaponDamage = weaponDamage;
 		this.armor = armor;
 		this.shield = shield;
 		this.heroAblty = heroAblty;
+	}
+	//Get start ---------------------
+	public String getHeroName()
+	{
+		return  heroName;
 	}
 	public float getHealth()
 	{
@@ -40,4 +50,14 @@ class Hero
 	{
 		return heroAblty;
 	}
+	//Get end ---------------------
+
+	//Methods start --------------
+	private void createAbility()
+	{
+		heroeAbilies.add(new Ability(/*read*/0.8,1.0,500));
+		heroeAbilies.add(new Ability(/*read*/0.8,1.0,500));
+		heroeAbilies.add(new Ability(/*read*/0.8,1.0,500));
+	}
+	//Methods end ----------------
 }
